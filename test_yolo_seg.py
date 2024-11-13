d9a2ee81d8409a87d5df22eca41b84ed3d583bc5
tungyen <cony998820@gmail.com>
2024-11-13 19:24:10 +0000

    Modify Confidence

--- test_yolo_seg.py
+++ test_yolo_seg.py
@@ -5,4 +5,4 @@ import cv2
 image_name = "/project/Pallets_segment/test/images/1523528-9928_jpg.rf.c3c0b7d079251f6ea4cfd4305e66e11b.jpg"
 ptName = os.path.join("runs", "segment", "train", "weights", "best.pt")
 model = YOLO(ptName)
-result = model(image_name, save=True)
+result = model(image_name, save=True, conf=0.5)
